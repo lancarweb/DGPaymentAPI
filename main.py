77f762aad2cd2a19a8af1978576d39c26af546cc
Lancarweb <lancarweboffice@gmail.com>
2022-09-13 15:11:13 +0700

    update dgp/api and add .env, .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+# web3py
+import json
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+
+# FastAPI
 from fastapi import FastAPI
+
+# load .env
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# profiders
+infura_url = "https://mainnet.infura.io/v3/06b3b50902f34272b0c1a09a08a71f18" # dgp
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# contract ABI DGP
+abi = json.loads(os.getenv("ABI"))
+
+# address DGP
+address = "0x927159670C50042109d7C0f4aEd0Cee89452433E"
+
+# contract DGP
+contract = web3.eth.contract(address=address, abi=abi)
 
 desc = """
 This API reference includes all technical documentation developers need to integrate third-party applications and platforms.
@@ -30,8 +54,12 @@ async def root():
 
 @app.get("/api/dgp")
 async def dgp_api():
+	totalSupply = contract.functions.totalSupply().call()
+
 	return {
-		"response": True
+		"totalsupply": str(web3.fromWei(totalSupply, 'ether')),
+		"name": contract.functions.name().call(),
+		"symbol": contract.functions.symbol().call()
 	}
 
 @app.get("/api/dgp/v1/balance")
